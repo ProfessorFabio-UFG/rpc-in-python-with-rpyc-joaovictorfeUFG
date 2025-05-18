a3120af05ba4231aa04efd1d68cbfaa3ae1037dc
joaovictorfeUFG <168030615+joaovictorfeUFG@users.noreply.github.com>
2025-05-18 17:38:24 -0300

    Update server.py

--- server.py
+++ server.py
@@ -12,6 +12,27 @@ class DBList(rpyc.Service):
   def exposed_value(self):
     return self.value
 
+  # soma todos os valores do array
+  def exposed_sum(self):
+    return sum(self.value)
+
+  # Remove todos os valores do array
+  def exposed_clear(self):
+    self.value = []
+    return self.value
+
+  # Remove o último item do array (se houver)
+  def exposed_pop(self):
+    if self.value:
+        self.value.pop()
+    return self.value
+
+  # Retorna o maior valor do array
+  def exposed_max(self):
+    if self.value:
+        return max(self.value)
+    return None
+
 if __name__ == "__main__":
   server = ThreadedServer(DBList(), port = PORT)
   server.start()
