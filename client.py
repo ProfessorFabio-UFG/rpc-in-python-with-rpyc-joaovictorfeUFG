d73f188f643f67e3ffb6c173e34ce16a30c641dc
joaovictorfeUFG <168030615+joaovictorfeUFG@users.noreply.github.com>
2025-05-18 17:43:47 -0300

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,21 @@ from constRPYC import * #-
 
 class Client:
   conn = rpyc.connect(SERVER, PORT) # Connect to the server
-  print (conn.root.exposed_value())
-  conn.root.exposed_append(5)       # Call an exposed operation,
-  conn.root.exposed_append(6)       # and append two elements
-  print (conn.root.exposed_value())   # Print the result
+  print ("Lista inicial:", conn.root.exposed_value())
+  print("Adicionando valores aleatórios:")
+    for _ in range(5):
+        n = random.randint(1, 10)
+        conn.root.exposed_append(n)
+        print(f"Adicionado: {n}")
+
+  print("Lista atual:", conn.root.exposed_value())
+  print("Soma:", conn.root.exposed_sum())
+  print("Maior valor:", conn.root.exposed_max())
+
+  # Remover o último item
+  conn.root.exposed_pop()
+  print("Após remover o último item:", conn.root.exposed_value())
+
+  # Remover todos os valores da lista
+  conn.root.exposed_clear()
+  print("Após remover todos os valores da lista:", conn.root.exposed_value())
